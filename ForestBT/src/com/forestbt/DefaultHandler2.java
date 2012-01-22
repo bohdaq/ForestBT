cd0ad1947757ccd85ab71dbb8d99ca74aeb4f8b8
Bogdan Tsap <bohdaq@gmail.com>
2012-01-23 01:03:53 +0200

    beta version

--- ForestBT/src/com/forestbt/DefaultHandler2.java
+++ ForestBT/src/com/forestbt/DefaultHandler2.java
@@ -31,6 +31,7 @@ public class DefaultHandler2 extends DefaultHandler
 	OakLeafVO tempOakLeafVO;
 	OakVO tempOakVO;
 	String tempValue;
+	int leavesNumber;
 
 	// resulting FOREST
 
@@ -40,6 +41,31 @@ public class DefaultHandler2 extends DefaultHandler
 	public void characters(char[] ch, int start, int length)
 			throws SAXException
 	{
+
+		// storing characters in tempValue
+		tempValue = new String(ch, start, length);
+
+		if (bId)
+		{
+			tempOakVO.setId(Long.parseLong(tempValue));
+			System.out.println("ID : " + tempValue);
+
+		}
+		if (bHeight && bLeafDetails == false)
+		{
+			tempOakVO.setHeight(Integer.parseInt(tempValue));
+			System.out.println("Height : " + tempValue);
+
+		}
+		if (bWidth && bLeafDetails == false)
+		{
+			tempOakVO.setWidth(Integer.parseInt(tempValue));
+			System.out.println("Width : " + tempValue);
+
+		}
+
+		// erasing it after use
+		tempValue = null;
 	}
 
 	@Override
@@ -52,6 +78,66 @@ public class DefaultHandler2 extends DefaultHandler
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException
 	{
+		// TREE processing
+		if (qName.equalsIgnoreCase("TREE"))
+		{
+			bTree = false;
+			tempOakVO = null;
+			System.out.println("\nTree nulled");
+		}
+		if (qName.equalsIgnoreCase("ID"))
+		{
+			bId = false;
+			System.out.println("ID FALSE");
+		}
+		if (qName.equalsIgnoreCase("HEIGHT") && bLeafDetails == false)
+		{
+			bHeight = false;
+			System.out.println("HEIGHT FALSE");
+		}
+		if (qName.equalsIgnoreCase("WIDTH") && bLeafDetails == false)
+		{
+			bWidth = false;
+			System.out.println("WIDTH FALSE");
+		}
+		if (qName.equalsIgnoreCase("AGE"))
+		{
+			bAge = false;
+			System.out.println("AGE FALSE");
+		}
+
+		// LEAF processing
+		if (qName.equalsIgnoreCase("LEAFDETAILS"))
+		{
+			bLeafDetails = false;
+			System.out.println("LEAFDETAILS FALSE");
+		}
+		if (qName.equalsIgnoreCase("LEAFCOUNT"))
+		{
+			bLeafCount = false;
+			System.out.println("LEAFCOUNT FALSE ");
+		}
+		if (qName.equalsIgnoreCase("LEAF"))
+		{
+			bLeaf = false;
+			tempOakLeafVO = null;
+			System.out.println("LEAF  DELETED");
+		}
+		if (qName.equalsIgnoreCase("COLOR"))
+		{
+			bColor = false;
+			System.out.println("COLOR  FALSE");
+		}
+		if (qName.equalsIgnoreCase("HEIGHT") && bLeafDetails == true)
+		{
+			bHeightLeaf = false;
+			System.out.println("HEIGHTLEAF  FALSE");
+		}
+		if (qName.equalsIgnoreCase("WIDTH") && bLeafDetails == true)
+		{
+			bWidthLeaf = false;
+			System.out.println("WIDTHLEAF  FALSE");
+		}
 	}
 
 	@Override
@@ -97,6 +183,32 @@ public class DefaultHandler2 extends DefaultHandler
 		{
 			bLeafDetails = true;
 			System.out.println("LEAFDETAILS TRUE");
+		}
+		if (qName.equalsIgnoreCase("LEAFCOUNT"))
+		{
+			bLeafCount = true;
+			System.out.println("LEAFCOUNT TRUE ");
+		}
+		if (qName.equalsIgnoreCase("LEAF"))
+		{
+			bLeaf = true;
+			tempOakLeafVO = new OakLeafVO();
+			System.out.println("LEAF  CREATED");
+		}
+		if (qName.equalsIgnoreCase("COLOR"))
+		{
+			bColor = true;
+			System.out.println("COLOR  TRUE");
+		}
+		if (qName.equalsIgnoreCase("HEIGHT") && bLeafDetails == true)
+		{
+			bHeightLeaf = true;
+			System.out.println("HEIGHTLEAF  TRUE");
+		}
+		if (qName.equalsIgnoreCase("WIDTH") && bLeafDetails == true)
+		{
+			bWidthLeaf = true;
+			System.out.println("WIDTHLEAF  TRUE");
 		}
 	}
 
